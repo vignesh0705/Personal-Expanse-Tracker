3c5b62d54b0dfc931f4ad4a0714d569b6c16c5c9
Vigneshvar pandi R <vigneshvarpandi@gmail.com>
2025-01-30 15:12:15 +0530

    edit the home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -12,15 +12,6 @@ function Home() {
           <Link to="/signup" className="btn">Sign Up</Link>
         </div>
       </div>
-      <div className="features">
-        <h2>Features</h2>
-        <ul>
-          <li>Track your daily expenses</li>
-          <li>Manage your income</li>
-          <li>Categorize transactions</li>
-          <li>View detailed reports</li>
-        </ul>
-      </div>
     </div>
   );
 }
